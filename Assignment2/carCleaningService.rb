3f0b0c3927d195103ae0201383e5f3902430a7e4
flaviuvr <flav.raita@gmail.com>
2021-04-20 18:14:05 +0300

    Add Station class

--- Assignment2/carCleaningService.rb
+++ Assignment2/carCleaningService.rb
@@ -1,3 +1,23 @@
+require 'time'
+
+module Schedule
+  START_WEEK_DAY = 1 # Monday
+  END_WEEK_DAY = 5 # Friday
+  START_TIME_HOUR = 8
+  END_TIME_HOUR = 17
+
+  def self.open?(time)
+    open_time?(time) && open_day?(time)
+  end
+
+  def self.open_time?(current_time)
+    current_time.hour > START_TIME_HOUR && current_time.hour < END_TIME_HOUR
+  end
+
+  def self.open_day?(current_day_of_week)
+    current_day_of_week.wday > START_WEEK_DAY && current_day_of_week.wday < END_WEEK_DAY
+  end
+end
 
 class User
   attr_reader :name, :car, :time_of_arrival, :time_of_pickup
@@ -44,3 +64,27 @@ class Car
   end
 
 end
+
+class Station
+  attr_accessor :name, :busy
+
+  def initialize(name)
+    @name = name
+    @busy = false
+  end
+
+  def start_clean(car)
+    puts "Now cleaning car #{car.license_plate_number} on #{name}" unless car.nil?
+    @busy = true
+  end
+
+  def end_clean(car)
+    puts "Done cleaning car #{car.license_plate_number} on #{name}" unless car.nil?
+  end
+
+  def clean_car(car)
+    start_clean(car)
+    end_clean(car)
+  end
+end
+
